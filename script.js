490d5e6aaa1d9b5c4d3b82edffc45b27ac450b57
Porkchopsss <pokoshih@gmail.com>
2020-06-22 23:03:26 +1000

    UV included

--- script.js
+++ script.js
@@ -6,62 +6,72 @@ $("#input-search").on("click", function (event) {
     retrieve();
 })
 
-$(document).on("click", ".newButtons",retrieve);
+$(document).on("click", ".newButtons", retrieve);
 
 // function that actually GETs the data returning with a response with corresponding citynames
-    function retrieve() {
-        $("#right-panel-top").empty();
-        $("#right-panel-bottom").empty();
-        var cityName = $("#userInput").val();
-        $(".list-group").append("<button id = \"" + cityName + "\"value=\"" + cityName + "\"type=\"button\" class=\"newButtons\">" + cityName + "</button>");
-        var apiKey = "c47a977ce2245fa14b93cb800e8c8a50";
-        // http would not work, uploading https instead
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                var temperature = response.main.temp;
-                var humidity = response.main.humidity;
-                var windSpeed = response.wind.speed;
-                var condition = response.weather[0].main;
-                if (condition == "clouds") {
+function retrieve() {
+    $("#right-panel-top").empty();
+    $("#right-panel-bottom").empty();
+    var cityName = $("#userInput").val();
+    $(".list-group").append("<button id = \"" + cityName + "\"value=\"" + cityName + "\"type=\"button\" class=\"newButtons\">" + cityName + "</button>");
+    var apiKey = "c47a977ce2245fa14b93cb800e8c8a50";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var temperature = response.main.temp;
+            var humidity = response.main.humidity;
+            var windSpeed = response.wind.speed;
+            var condition = response.weather[0].main;
+            if (condition == "clouds") {
+                var conditionIcon = "http://openweathermap.org/img/wn/03d@2x.png";
+            } else if (condition == "clear") {
+                var conditionIcon = "http://openweathermap.org/img/wn/01d@2x.png";
+            } else {
+                var conditionIcon = "http://openweathermap.org/img/wn/09d@2x.png";
+            }
+            var pasteT = "Temperature: " + temperature + " &#730C<br>" +
+                "Humidity: " + humidity + " %<br>" +
+                "Wind speed: " + windSpeed + " KMh<br>";
+            $("#right-panel-top").append("<h2>" + cityName + "</h2>" + "<img class=\"conditionIcon\" src=" + conditionIcon + ">" + condition + "<br>");
+            $("#right-panel-top").append(pasteT);
+            var cityLat = response.coord.lat;
+            var cityLon = response.coord.lon;
+
+            var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + apiKey;
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+                .then(function (response) {
+                    var uvVal = response.value;
+                    $("#right-panel-top").append( "UV Index: " + uvVal);
+                });
+        });
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",au" + "&units=metric&appid=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            $("#right-panel-bottom").append("<div class=\"forecastTitle\" >5Day Forecast</div>" + "<br>" + "<div class=\"follow\"></div>");
+            for (i = 0; i < response.list.length; i += 8) {
+                var date = response.list[i].dt_txt.slice(0, 11);
+                var temperature = response.list[i].main.temp;
+                var humidity = response.list[i].main.humidity;
+                var weatherMain = response.list[i].weather[0].main;
+                if (weatherMain == "clouds") {
                     var conditionIcon = "http://openweathermap.org/img/wn/03d@2x.png";
-                } else if (condition == "clear") {
+                } else if (weatherMain == "clear") {
                     var conditionIcon = "http://openweathermap.org/img/wn/01d@2x.png";
                 } else {
                     var conditionIcon = "http://openweathermap.org/img/wn/09d@2x.png";
                 }
-                var pasteT = "Temperature: " + temperature + " &#730C<br>" +
-                    "Humidity: " + humidity + " %<br>" +
-                    "Wind speed: " + windSpeed + " KMh<br>";
-                $("#right-panel-top").append("<h2>" + cityName + "</h2>" + "<img class=\"conditionIcon\" src=" + conditionIcon + ">" + condition + "<br>");
-                $("#right-panel-top").append(pasteT);
-                console.log(response);
-            });
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",au" + "&units=metric&appid=" + apiKey;
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                $("#right-panel-bottom").append("<div class=\"forecastTitle\" >5Day Forecast</div>" + "<br>" + "<div class=\"follow\"></div>");
-                for (i = 0; i < response.list.length; i += 8) {
-                    var date = response.list[i].dt_txt.slice(0, 11);
-                    var temperature = response.list[i].main.temp;
-                    var humidity = response.list[i].main.humidity;
-                    var weatherMain = response.list[i].weather[0].main;
-                    if (weatherMain == "clouds") {
-                        var conditionIcon = "http://openweathermap.org/img/wn/03d@2x.png";
-                    } else if (weatherMain == "clear") {
-                        var conditionIcon = "http://openweathermap.org/img/wn/01d@2x.png";
-                    } else {
-                        var conditionIcon = "http://openweathermap.org/img/wn/09d@2x.png";
-                    }
-                    var pasteT = "<div  class=\"col-md-2 dailyForecast\">" + date + "<img class=\"conditionIcon\" src=" + conditionIcon + ">" + weatherMain + "<br>" + "Temp: " + temperature + " &#730C <br>"
-                        + "Humidity: " + humidity + "</div>";
-                    $(".follow").append(pasteT);
-                };
-            });
-    };
+                var pasteT = "<div  class=\"col-md-2 dailyForecast\">" + date + "<img class=\"conditionIcon\" src=" + conditionIcon + ">" + weatherMain + "<br>" + "Temp: " + temperature + " &#730C <br>"
+                    + "Humidity: " + humidity + "</div>";
+                $(".follow").append(pasteT);
+            };
+        });
+};
